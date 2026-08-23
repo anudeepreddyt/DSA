75b37b7607342a06f1a3904aea284a57bf573e32
Anudeep <anudeepreddy4637@gmail.com>
2026-08-23 23:38:50 +0530

    Time: 21 ms (29.76%) | Memory: 44.6 MB (30.57%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,5 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:s1.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int left=0,count=s1.length();
+
+        for(int right=0;right<s2.length();right++){
+            char ch=s2.charAt(right);
+
+            int val=map.getOrDefault(ch,0);
+            if(val>0) count--;
+            map.put(ch,val-1);
+
+            if(count==0) return true;
+
+            if(right-left+1 == s1.length()){
+                char leftChar=s2.charAt(left);
+                int leftVal=map.get(leftChar);
+                if(leftVal>=0) count++;
+                map.put(leftChar,leftVal+1);
+                left++;
+            }
+
+        }
+        return false;
+    }
+}
+
+/*
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    if(s1.length()>s2.length()) return false;
         int[] freq1=new int[26];
         int[] freq2=new int[26];
 
@@ -26,10 +59,12 @@ class Solution {
         return false;
     }
 }
+*/
 
 /*
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+    if(s1.length()>s2.length()) return false;
         HashMap<Character,Integer> map=new HashMap<>();
         for(char c:s1.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
